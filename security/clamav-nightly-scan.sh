3d76556d62f15d9c268c7b6562027278b3ec4ebe
77777kaz77777 <153332224+77777kaz77777@users.noreply.github.com>
2026-08-02 21:04:48 -0400

    Update and rename clamav to clamav-nightly-scan.sh

---
+++ security/clamav-nightly-scan.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+LOGFILE="/var/log/clamav/daily_scan.log"
+
+# Create log folder if missing
+mkdir -p /var/log/clamav
+
+echo "=== Scan Started: $(date) ===" >> "$LOGFILE"
+clamdscan --fdpass --infected --multiscan --log="$LOGFILE" /home /usr/bin /usr/local/bin
+echo "=== Scan Finished: $(date) ===" >> "$LOGFILE"
