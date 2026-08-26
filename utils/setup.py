a497c4003d3c8ef5260760ce3a56546406bb3e40
kaz <153332224+77777kaz77777@users.noreply.github.com>
2026-08-26 17:50:46 -0400

    Update setup.py

--- utils/setup.py
+++ utils/setup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Automated Python workstation bootstrap, toolstack installer, and repository script deployment.
+# (work in progress )Automated Python workstation bootstrap, toolstack installer, and repository script deployment.
 import os
 import sys
 import subprocess
